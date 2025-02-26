ec7794d51f42383f114cdec531595b17feb17c2e
Himani416 <himanisharma1604@gmail.com>
2025-02-26 19:20:38 +0530

    password

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+let inputEl=document.querySelector("input");
+let showEl=document.querySelector(".fa-eye");
+let hideEl=document.querySelector(".fa-eye-slash");
+
+showEl.addEventListener("click",() =>{
+    inputEl.type="text";
+    hideEl.classList.remove("hide");
+    showEl.classList.add("hide");
+})
+
+hideEl.addEventListener("click" ,() =>{
+    inputEl.type="password";
+    hideEl.classList.add("hide");
+    showEl.classList.remove("hide");
+})
+
 const signUpButton=document.getElementById('signUpButton');
 const signInButton=document.getElementById('signInButton');
 const signInForm=document.getElementById('Sign In');
