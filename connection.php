c8887fbe2dc04277e192167b203f870429d03af0
KM Meena <janki.runkuni121@gmail.com>
2025-03-15 15:44:23 +0530

    for signin checkup

--- connection.php
+++ connection.php
@@ -1,29 +1,34 @@
 <?php
-if(isset($_POST['name'])){
+session_start();
     $server="localhost";
     $username="root";
     $password="" ;
-    $database="printing design";
-
+    $database="printing_design";
     $con= mysqli_connect($server ,$username,$password,$database);
-
-    if(!$con){
-        die("connection to this databse failed due to". mysqli_connect_error());
+    if (!$con) {
+        die("Connection failed: " .mysqli_connect_error());
+    }else {
+        echo "Connected successfully";
     }
-    //echo " Success connecting to the db";
-
-    $name = $_POST['name'] ?? '';
-    $email = $_POST['email'] ?? '';
-    $password = $_POST['password'] ?? '';
-    $sql= "INSERT INTO user(`name`, `email`, `password`) VALUES ( '$name', '$email', '$password');";
-    //echo $sql;
-
-    if($con->query($sql) == true){
-        echo "Sucessfully recorded the response. Thank you!";
-    }
-    else{
-        echo"Error: $sql <br> $con->error";
-    }
-    $con->close();
+    
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    if(isset($_POST['sign In'])){
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    $password=md5($password) ;
+   
+    $sql="SELECT * FROM users WHERE email='$email' and password='$password'";
+    $result=$con->query($sql);
+    if($result->num_rows>0){
+    $row=$result->fetch_assoc();
+    $_SESSION['email']=$row['email'];
+    header("Location: design-canvas.html");
+    exit();
+   }
+   else{
+    echo "Not Found, Incorrect Email or Password";
+   }
 }
+}
+$con->close();
 ?>
