b64fcd489dc5b8e79efa131f4f66802183f4f834
bhumiip06 <bhumikapimoli87@gmail.com>
2025-03-26 12:22:51 +0530

    updated script with pdo initialisation  for mysql

--- connection.php
+++ connection.php
@@ -1,33 +1,21 @@
 <?php
-session_start();
+// session_start();
     $server="localhost";
     $username="root";
     $password="" ;
     $database="printing design";
     $con= mysqli_connect($server ,$username,$password,$database);
-    if (!$con) {
-        die("Connection failed: " .mysqli_connect_error());
-    }else {
-        echo "Connected successfully";
+    // if (!$con) {
+    //     die("Connection failed: " .mysqli_connect_error());
+    // }else {
+    //     echo "Connected successfully";
+    // }
+//PDO initialize
+    try{
+        $pdo=new PDO("mysql:host=$server;dbname=$database",$username,$password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    if(isset($_POST['sign In'])){
-    $email=$_POST['email'];
-    $password=$_POST['password'];
-    $password=md5($password) ;
-   
-    $sql="SELECT * FROM users WHERE email='$email' and password='$password'";
-    $result=$con->query($sql);
-    if($result->num_rows>0){
-    $row=$result->fetch_assoc();
-    $_SESSION['email']=$row['email'];
-    header("Location: design-canvas.html");
-    exit();
-   }
-   else{
-    echo "Not Found, Incorrect Email or Password";
-   }
-}
-}
-// $con->close();
+    catch(PDOEXCEPTION $e){
+        die("Connection failed:".$e->getMessage());
+    }
 ?>
