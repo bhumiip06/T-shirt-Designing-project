7787e21a12dc4fb66c76c095fcbe91889ff33ed0
bhumiip06 <bhumikapimoli87@gmail.com>
2025-04-28 20:47:24 +0530

    logic for checking if user is logged in or not using json response

---
+++ php/check_session.php
@@ -0,0 +1,13 @@
+<?php
+session_start();  // Start the session
+
+// Check if the user is logged in
+if (!isset($_SESSION['username'])) {
+    // If not logged in, return a JSON response indicating the user is not logged in
+    echo json_encode(['loggedIn' => false]);
+    exit();
+}
+
+// If the user is logged in, return a JSON response indicating the user is logged in
+echo json_encode(['loggedIn' => true, 'username' => $_SESSION['username']]);
+?>
