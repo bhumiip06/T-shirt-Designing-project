a24559995fbb80a328815555e7a8d4433cff489d
bhumiip06 <bhumikapimoli87@gmail.com>
2025-05-03 13:28:32 +0530

    user id is set in session varibles so that designs can be saved with user id to designs table in database

--- php/login.php
+++ php/login.php
@@ -6,7 +6,7 @@ if($_SERVER['REQUEST_METHOD']==='POST'){
     if(isset($_POST['email']) && isset($_POST['login_password']))
     $email=$_POST['email'];
     $password=$_POST['login_password'];
-    $sql="SELECT user_password,user_name FROM users where user_email=:email";
+    $sql="SELECT user_id,user_password,user_name FROM users where user_email=:email";
     $stmt=$pdo->prepare($sql);
     $stmt->bindParam(':email',$email);
 
@@ -17,6 +17,7 @@ if($_SERVER['REQUEST_METHOD']==='POST'){
 
         if(password_verify($password,$user['user_password'])){
            // echo"Login Successful";
+           $_SESSION['user_id']=$user['user_id'];
            $_SESSION['username']=$user['user_name']; //sets username in the session
            $_SESSION['user_email']=$email; //sets email in the session
            
